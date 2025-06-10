342df3afe33d3a34b007c16c31fa5b1c4f6fbbce
Vasil Suarez <vasil.zabuza.luffy@gmail.com>
2025-06-10 08:42:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,4 @@ import os
 class Config:
     KEY_db=os.getenv('KEY')
     SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{KEY_db}@localhost:5432/data_base"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:f4_hello_binbon@localhost:5432/data_base"
-
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
